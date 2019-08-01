e05dd84c63676151a92367e0d806307018d4026a
Natalie Taktachev <ntaktach@gmail.com>
2019-08-01 23:57:07 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def add_song(song)
+     song.artist = self
+   end
+
+   def songs
+     Song.all.select {|song| song.artist == self}
+   end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+  self.artist.name
+end
+
+end
