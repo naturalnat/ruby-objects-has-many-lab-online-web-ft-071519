3d8f19f5d47e335ce3ca894e812fe3bd8c64cc9f
Natalie Taktachev <ntaktach@gmail.com>
2019-08-02 00:06:47 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,15 @@
 class Author
-  attr_accessor :name
+  attr_accessor :name, :post, :title
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def add_post_by_title(title)
+  post = Post.new(title)
+  post.author = self 
   end
 end
